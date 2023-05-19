f1c846c5997bcca7817f4f9027881d687388eb8a
Sven Wappler <info@wappler.systems>
2023-05-19 03:02:11 +0200

    first commit

---
+++ Configuration/JavaScriptModules.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+    'dependencies' => ['backend'],
+    'tags' => [
+        'backend.form',
+    ],
+    'imports' => [
+        '@wapplersystems/rt-ckeditor-translations/' => 'EXT:rte_ckeditor_translations/Resources/Public/JavaScript/translations/',
+    ],
+];
